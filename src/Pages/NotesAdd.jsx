73e106b6e311715ce49efcd9ba02c1f8cfa9de91
Rushish <mewadarushish@gmail.com>
2023-04-01 14:09:47 +0530

    Empty

--- src/Pages/NotesAdd.jsx
+++ src/Pages/NotesAdd.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Link } from "react-router-dom"
+import "../assets/Styles/notesadd.css"
 
 function Table() {
   const formDataArray = JSON.parse(localStorage.getItem('addClient')) || [];
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Sr No.</th>
-          <th>Notes and Deviatation</th>
-        </tr>
-      </thead>
-      <tbody>
-        {formDataArray.map((quotationData, index) => (
-          <tr key={index}>
-            <td>{quotationData.companyName}</td>
-            <td className='button-wrap'><Link to="/create-quotation"><button className='create-quote'>Create Quotation</button></Link></td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className='bodywrap'>
+      <h1>Notes And Deviations</h1>
+      <textarea name="notesarea"></textarea>
+      <div className='table-wrap'>
+        <table>
+          <thead>
+            <tr>
+              <th>Sr No.</th>
+              <th>Notes and Deviatation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {formDataArray.map((quotationData, index) => (
+              <tr key={index}>
+                <td>{quotationData.companyName}</td>
+                <td className='button-wrap'><Link to="/create-quotation"><button className='create-quote'>Create Quotation</button></Link></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
