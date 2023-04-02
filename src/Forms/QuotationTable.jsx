14e2026d9c53a8709e3db8caf2727259128ed0ce
Aryan Shinde <shindearyan179@gmail.com>
2023-04-02 13:04:38 +0530

    Fetch and Show Data

--- src/Forms/QuotationTable.jsx
+++ src/Forms/QuotationTable.jsx
@@ -1,27 +1,51 @@
-import React from 'react';
-import { Link } from "react-router-dom"
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Table() {
-    const formDataArray = JSON.parse(localStorage.getItem('addClient')) || [];
+  const [clientsData, setClientsData] = useState([]);
+  const formDataArray = JSON.parse(localStorage.getItem("addClient")) || [];
 
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Company Name</th>
-                    <th>Create Quotation</th>
-                </tr>
-            </thead>
-            <tbody>
-                {formDataArray.map((quotationData, index) => (
-                    <tr key={index}>
-                        <td>{quotationData.companyName}</td>
-                        <td className='button-wrap'><Link to="/create-quotation"><button className='create-quote'>Create Quotation</button></Link></td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
+  axios({
+    method: "get",
+    url: "http://localhost:8080/get-clients",
+  })
+    .then((resp) => {
+      //   console.log(resp);
+      setClientsData(resp.data);
+    })
+    .catch((err) => console.log(err));
+
+  const createQuotation = (id) => {
+    // e.preventDefault();
+    console.log(id);
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Company Name</th>
+          <th>Create Quotation</th>
+        </tr>
+      </thead>
+      <tbody>
+        {clientsData.map((quotationData, index) => (
+          <tr key={index}>
+            <td>{quotationData.company_name}</td>
+            <td className="button-wrap">
+              <button
+                className="create-quote"
+                onClick={() => createQuotation(quotationData.id)}
+              >
+                Create Quotation
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
